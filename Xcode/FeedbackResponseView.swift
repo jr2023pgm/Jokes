bda55b1d542d38087fec89e8b3f6d6c9fbb80030
Jay Ram <jr2023pgm@gmail.com>
2023-07-05 16:12:01 +0400

    Added pictures

--- Xcode/FeedbackResponseView.swift
+++ Xcode/FeedbackResponseView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct FeedbackResponseView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("happy")
+                .resizable()
+                .scaledToFit()
+            Text("Thanks, here's a cookie 🍪!")
+                .padding()
+        }
     }
 }
 
